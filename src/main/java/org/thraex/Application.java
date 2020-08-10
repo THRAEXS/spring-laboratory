3e78b6b05373a6000c211d68d45bd44e332ecf29
鬼王 <THRAEX@aliyun.com>
2020-08-10 17:44:25 +0800

    chore: add init class

---
+++ src/main/java/org/thraex/Application.java
@@ -0,0 +1,17 @@
+package org.thraex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author 鬼王
+ * @date 2020/08/10 17:40
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
