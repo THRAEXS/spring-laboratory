8fcef030a861d6f1f65ce30a97695e463ed1ae03
鬼王 <THRAEX@aliyun.com>
2020-08-11 20:16:45 +0800

    fix: thymeleaf config

--- src/main/java/org/thraex/config/MvcConfig.java
+++ src/main/java/org/thraex/config/MvcConfig.java
@@ -13,9 +13,6 @@ public class MvcConfig implements WebMvcConfigurer {
 
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
-        registry.addViewController("/").setViewName("home");
-        registry.addViewController("/home").setViewName("home");
-        registry.addViewController("/hello").setViewName("hello");
         //registry.addViewController("/login").setViewName("login");
     }
 }
