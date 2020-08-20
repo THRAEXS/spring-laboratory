cfedef51a3df782e829ab964d02f081ba8849ea1
鬼王 <THRAEX@aliyun.com>
2020-08-20 15:44:13 +0800

    opt: thymeleaf head and script

--- src/main/java/org/thraex/platform/controller/MenuController.java
+++ src/main/java/org/thraex/platform/controller/MenuController.java
@@ -1,6 +1,7 @@
 package org.thraex.platform.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -10,5 +11,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("menu")
 public class MenuController {
+
+    @GetMapping
+    public String index() {
+        // TODO: menu tree
+        return "menu/index";
+    }
+
+    @GetMapping("edit")
+    public String edit() {
+        return "menu/edit";
+    }
 
 }
