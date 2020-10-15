0d4e72eec63bd5a03a51eef0d8615ff4c7ddb34c
鬼王 <THRAEX@aliyun.com>
2020-10-15 09:35:23 +0800

    fix: login redirect

--- src/main/java/org/thraex/security/handler/AuthenticationSuccess.java
+++ src/main/java/org/thraex/security/handler/AuthenticationSuccess.java
@@ -1,16 +1,20 @@
 package org.thraex.security.handler;
 
 import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
+import org.thraex.base.properties.SiteProperties;
 import org.thraex.base.result.Result;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @author 鬼王
@@ -20,12 +24,18 @@ import java.io.IOException;
 @Component
 public class AuthenticationSuccess extends SavedRequestAwareAuthenticationSuccessHandler {
 
+    @Autowired
+    private SiteProperties sitProperties;
+
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
                                         Authentication authentication) throws ServletException, IOException {
         log.debug("Authentication success");
 
-        JSONWriter.write(response, HttpStatus.OK, Result.ok().setMessage("Authentication success"));
+        // TODO: Optimize
+        Map<String, Object> result = new HashMap<>(1);
+        result.put("redirect", sitProperties.redirect());
+        JSONWriter.write(response, HttpStatus.OK, Result.ok(result));
     }
 
 }
